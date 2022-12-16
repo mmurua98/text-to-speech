70cd7907ae508de3288e72a389cd2567b5b5e8a7
mmurua98 <55469486+mmurua98@users.noreply.github.com>
2022-12-16 16:01:32 -0700

    delete audio

--- app.py
+++ app.py
@@ -18,20 +18,33 @@ def convertText():
     # audioName = converter.getAudioName()
     # pathFile = pathDir + '\\' + audioName
     # print(pathFile)
+    global pathDir, origin, target
     audioName = converter.getAudioName()
     #origin = "/textspeech/"+audioName
     pathDir = os.getcwd()
     origin = pathDir + '\\' + audioName
     target = os.path.join(pathDir, 'static\\audio', audioName)
 
-    print(origin)
-    print(target)
+    # print(origin)
+    # print(target)
 
     if os.path.exists(origin):
         shutil.move(origin, target)
     
     return render_template('index.html')
 
+
+@app.after_request
+def deleteVideo(response):
+    if request.endpoint=="converter":
+        if os.path.exists(target):
+            try:
+                #print(target)
+                os.remove(target)
+            except OSError as e: # name the Exception `e`
+                print ("Failed with:", e.strerror)# look what it says
+                print ("Error code:", e.errno )
+    return response
 
 # @app.route("/getaudio", methods=['POST', 'GET'])
 # def showAudio():
