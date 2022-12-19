13410f7b7592efbaaa45bfa3467dc9106c474a05
mmurua98 <55469486+mmurua98@users.noreply.github.com>
2022-12-19 15:14:39 -0700

    cors fixed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from whitenoise import WhiteNoise
 import converter
 import os
@@ -7,12 +7,13 @@ import shutil
 from mutagen.mp3 import MP3
 
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
-# CORS(app)
+#app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
 # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-
+@cross_origin()
 @app.route("/")
 def home():
     return render_template('index.html')
