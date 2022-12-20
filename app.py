9dae4f7a8c97fa37ed4a426a3a25137b8d1b6854
mmurua98 <55469486+mmurua98@users.noreply.github.com>
2022-12-20 10:59:43 -0700

    update

--- app.py
+++ app.py
@@ -8,12 +8,8 @@ from mutagen.mp3 import MP3
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-# app.config['TEMPLATES_AUTO_RELOAD'] = True
-# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@cross_origin()
+
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -24,7 +20,7 @@ def convertText():
     converter.getText(txtInput)
 
 
-    global pathDir, origin, target, audioDuration, audioFilePath
+    global pathDir, origin, target, audioDuration, audioFilePath, audioName
     audioName = converter.getAudioName()
 
     pathDir = os.getcwd()
@@ -45,8 +41,7 @@ def convertText():
 
 @app.route("/getaudio")
 def getAudio():
-    #print(audioFilePath)
-    return render_template('audio.html', pathAudio=audioFilePath), {"Refresh": ""+audioDuration+"; url=/"}
+    return render_template('audio.html', pathAudio=audioName), {"Refresh": ""+audioDuration+"; url=/"}
 
 
 
